135a3544fa9799c0d8eec448ba9711ee2ba6d40e
村田花穂 <muratahanaho@muratakasuinoMacBook-Air.local>
2020-04-28 02:02:47 +0900

    Start 4-6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,7 +4,7 @@ class BlogsController < ApplicationController
   end
 
   def show
-    @blog = Blog.find(params[:id])
+      @blog = Blog.find(params[:id])
   end
 
   def new
@@ -18,6 +18,13 @@ class BlogsController < ApplicationController
   end
 
   def edit
+      @blog = Blog.find(params[:id])
+  end
+
+  def update
+      blog = Blog.find(params[:id])
+      blog.update(blog_params)
+      redirect_to blog_path(blog)
   end
   
   def blog_params
